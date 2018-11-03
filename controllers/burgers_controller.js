8affcda7f487ad1034b8bbe449847311d0528e25
Johnny Zavala <jzavalafgz@gmail.com>
2018-11-03 00:38:51 -0400

    finsihed setting up controller

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,33 @@
+var express = require(express);
+var router = express.Router();
+var burger = require("../models/burger.js");
+
+router.get('/', function (req, res) {
+    res.redirect('/burgers');
+});
+
+router.get('/burgers', function (req, res) {
+    burger.all(function (data) {
+        var hbsObject = { burgers: data };
+        console.log(hbsObject);
+        res.render('index', hbsObject);
+    });
+});
+
+router.post('/burgers/create', function (req, res) {
+    burger.create(['burger_name', 'devoured'], [req.body.burgers_name, req.body.devoured], function () {
+        res.redirect('/burgers');
+    });
+});
+
+router.put('/cats/update/:id', function (req, res) {
+    var condition = 'id = ' + req.params.id;
+
+    console.log('condition', condition);
+
+    burger.update({ burgers_name: req.body.burgers_name }, condition, function () {
+        res.redirect('/cats');
+    });
+});
+
+module.exports = router;
